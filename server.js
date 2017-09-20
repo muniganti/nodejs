ccd52a09fef5d8cbea2e57ce1302a18e4875c4e7
Vinay Muniganti <vmuniganti@walmartlabs.com>
2017-09-20 00:13:13 -0700

    added script for npm

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname+"/views/partials");
@@ -42,6 +43,6 @@ app.get('/bad', (req, res) => {
 	});
 })
 
-app.listen(3000, () => {
-	console.log("Server started at 3000");
+app.listen(port, () => {
+	console.log(`Server started at ${port}`);
 });
